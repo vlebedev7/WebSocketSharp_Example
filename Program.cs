be6b76390e5ea1ab9c264cd23cb11fd5ec5f9011
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2021-10-20 00:24:02 +0600

    add Send method, add comments

--- Program.cs
+++ Program.cs
@@ -7,22 +7,33 @@ namespace WebSocketsExample
 {
     class Program
     {
+        private static WebSocketServer _wsServer;
+
         static void Main(string[] args)
         {
-            var wsServer = new WebSocketServer(4649);
-            wsServer.AddWebSocketService<WSDeals>("/deals");
-            wsServer.Start();
-            Console.ReadKey();
-            wsServer.Stop();
+            _wsServer = new WebSocketServer(4649);
+            _wsServer.AddWebSocketService<WSDeals>("/deals"); // Url: ws://localhost:4649/deals
+            _wsServer.Start();
+            Console.ReadKey(true);
+
+            Send("4444");
+            _wsServer.Stop();
         }
 
         public class WSDeals : WebSocketBehavior
         {
-            protected override void OnMessage(MessageEventArgs e)
+            protected override void OnMessage(MessageEventArgs e) // process message from client
             {
                 var msg = e.Data == "abc" ? "response1" : "response2";
-                Send(msg);
+                Send(msg); // respond to client
             }
+        }
+
+        public static void Send(string data) // custom message from server to client
+        {
+            var endpoint = _wsServer.WebSocketServices["/deals"];
+            var sessionId = endpoint.Sessions.ActiveIDs.First();
+            endpoint.Sessions.SendTo(data, sessionId);
         }
     }
 }
