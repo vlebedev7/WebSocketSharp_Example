8adec0940ea8ba60061948f0ca5b1ef2f9eb1987
Vitaliy Lebedev <vitaliy.lebedev7@gmail.com>
2021-10-23 11:57:24 +0600

    save sessionIds, send message to all

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using WebSocketSharp;
 using WebSocketSharp.Server;
@@ -8,15 +10,17 @@ namespace WebSocketsExample
     class Program
     {
         private static WebSocketServer _wsServer;
+        private static ConcurrentDictionary<string, byte> _sessions = new();
 
         static void Main(string[] args)
         {
+            // https://github.com/sta/websocket-sharp
             _wsServer = new WebSocketServer(4649);
             _wsServer.AddWebSocketService<WSDeals>("/deals"); // Url: ws://localhost:4649/deals
             _wsServer.Start();
             Console.ReadKey(true);
 
-            Send("4444");
+            SendToAllSessions("some text");
             _wsServer.Stop();
         }
 
@@ -24,16 +28,30 @@ namespace WebSocketsExample
         {
             protected override void OnMessage(MessageEventArgs e) // process message from client
             {
+                _sessions.AddOrUpdate(ID, 0, (key, oldValue) => { return 0; }); // save sessionId
+
                 var msg = e.Data == "abc" ? "response1" : "response2";
                 Send(msg); // respond to client
             }
         }
 
-        public static void Send(string data) // custom message from server to client
+        public static WebSocketServiceHost GetEndpoint() =>
+            _wsServer.WebSocketServices["/deals"];
+
+        public static void SendToSession(string sessionId, string data) // custom message from server to client
         {
-            var endpoint = _wsServer.WebSocketServices["/deals"];
-            var sessionId = endpoint.Sessions.ActiveIDs.First();
-            endpoint.Sessions.SendTo(data, sessionId);
+            GetEndpoint().Sessions.SendTo(data, sessionId);
+        }
+        public static void SendToAllSessions(string data) // custom message from server to all clients
+        {
+            foreach (var sessionId in _sessions.Keys)
+                SendToSession(sessionId, data); 
+        }
+
+        public static List<string> GetActiveSessions()
+        {
+            var sessions = GetEndpoint().Sessions.ActiveIDs;
+            return sessions.ToList();
         }
     }
 }
